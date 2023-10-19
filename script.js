1c3db28a8f4f6ead3e9c88aa948395f1e8c391bb
mangeshahireakaXylus11 <97844580+xylus11@users.noreply.github.com>
2023-10-19 23:21:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function threeSum(arr, target) {
 // write your code here
+	let a= arr.length;
+	let sum =0;
+	let i =0;
+	while(i<a)
+		{  
+			if(sum<=target)
+		{
+			sum = arr[i]+sum;
+			i++;
+		}
+		 else{
+			 i++;
+		 }
+			
+		}
+	return sum;
   
 }
 
