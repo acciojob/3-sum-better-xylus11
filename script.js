38a58f8bbee280abbc7e5cfce141639a3ba8733d
mangeshahireakaXylus11 <97844580+xylus11@users.noreply.github.com>
2023-10-19 23:32:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,35 @@
-function threeSum(arr, target) {
-// write your code here
-	let a= arr.length;
-	let sum =0;
-	let i =0;
-	while(i<a)
-		{  
-			if(sum<=target)
-		{
-			sum = arr[i]+sum;
-		}
-		 else{
-			 i++;
-		 }
-			i++;
-		}
-	return sum;
+function threeSum(nums, target) {
+nums.sort((a, b) => a - b);
+
+  // Initialize variables to store the closest sum and the minimum difference
+  let closestSum = Infinity;
+  let minDiff = Infinity;
+
+  for (let i = 0; i < nums.length - 2; i++) {
+    let left = i + 1;
+    let right = nums.length - 1;
+
+    while (left < right) {
+      const sum = nums[i] + nums[left] + nums[right];
+      const diff = Math.abs(sum - target);
+
+      if (diff < minDiff) {
+        minDiff = diff;
+        closestSum = sum;
+      }
+
+      if (sum < target) {
+        left++;
+      } else if (sum > target) {
+        right--;
+      } else {
+        // If the sum equals the target, return it immediately (closest possible sum)
+        return sum;
+      }
+    }
+  }
+
+  return closestSum;
   
 }
 
